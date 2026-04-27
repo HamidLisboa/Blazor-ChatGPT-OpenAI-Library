a42889866af7e075fca86e7b2f5f10ece71a0aac
HamidLisboa <hrh60916@gmail.com>
2026-04-27 14:48:40 +0100

    Add image generation method in back-end and front-end

--- BlazorChatGPT/Services/GPTService.cs
+++ BlazorChatGPT/Services/GPTService.cs
@@ -1,4 +1,5 @@
 ﻿using OpenAI.Chat;
+using OpenAI.Images;
 using System.ClientModel;
 namespace BlazorChatGPT.Services
 {
@@ -6,29 +7,20 @@ namespace BlazorChatGPT.Services
     {
         #region [00] Shared Variables
         private ChatClient _chatClient;
+        private ImageClient _imagelient;
         #endregion
         #region [01] Constractor of GPTService
-        public GPTService(string gptModel="gpt-4o-mini")
+        public GPTService(string gptModel="gpt-4o-mini", string imageModel="dall-e-3")
         {
             _chatClient = new ChatClient(
                 model: gptModel,
                 apiKey: Environment.GetEnvironmentVariable("OPENAI_APIKEY"));
-             
+            _imagelient = new (
+                model: imageModel,
+                apiKey: Environment.GetEnvironmentVariable("OPENAI_APIKEY"));
         }
         #endregion
-        #region [02] Chat Completion method - non-streaming mode
-        //public ChatCompletion GetResponse(string prompt)
-        //{
-        //    ChatCompletion result= _chatClient.CompleteChat(prompt);
-        //    return result;
-        //}   
-        //public async Task<ChatCompletion> GetResponseAsync(string prompt)
-        //{
-        //    ChatCompletion result= await _chatClient.CompleteChatAsync(prompt);
-        //    return result;
-        //}
-        #endregion
-        #region [03] Chat Completion method - streaming mode
+        #region [02] Chat Completion method - streaming mode
         public AsyncCollectionResult<StreamingChatCompletionUpdate> GetStreamingResponseAsync(string prompt)
         {
             ChatCompletionOptions options = new ChatCompletionOptions();
@@ -40,6 +32,13 @@ namespace BlazorChatGPT.Services
             AsyncCollectionResult<StreamingChatCompletionUpdate> completionUpdates = 
                 _chatClient.CompleteChatStreamingAsync(messages, options);
             return completionUpdates;
+        }
+        #endregion
+        #region [03] Generating Images using OpenAI´s Image API
+        public async Task<GeneratedImage> GenerateImageAsync(string prompt)
+        {
+            GeneratedImage image = await _imagelient.GenerateImageAsync(prompt);
+            return image;
         }
         #endregion
     }
