19bf3fedc3b7c600718cd67ab191544d4317a90a
HamidLisboa <hrh60916@gmail.com>
2026-05-02 16:57:44 +0100

    Improving Generation Methods by JS Interop

--- BlazorChatGPT/wwwroot/js/site.js
+++ BlazorChatGPT/wwwroot/js/site.js
@@ -4,3 +4,11 @@
         chatMessages.scrollTop = chatMessages.scrollHeight;
     }
 }
+window.setImageModalInvoker = function (dotNetObjRef) {
+    window.setImageModalInvoker = dotNetObjRef;
+}
+function openModalFromJS = function (imageSrc) {
+    if (window.setImageModalInvoker) {
+        window.setImageModalInvoker.invokeMethodAsync('OpenModal', imageSrc);
+    }
+}
