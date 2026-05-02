750bcd3c2f66f98fe08617dbc2a029d94e1c738a
HamidLisboa <hrh60916@gmail.com>
2026-05-02 13:41:12 +0100

    Generate  image base64

--- BlazorChatGPT/Services/GPTService.cs
+++ BlazorChatGPT/Services/GPTService.cs
@@ -1,23 +1,27 @@
 ﻿using OpenAI.Chat;
 using OpenAI.Images;
 using System.ClientModel;
+using Microsoft.AspNetCore.Hosting;
+
 namespace BlazorChatGPT.Services
 {
     public class GPTService
     {
         #region [00] Shared Variables
         private ChatClient _chatClient;
-        private ImageClient _imagelient;
+        private ImageClient _imageclient;
+
         #endregion
         #region [01] Constractor of GPTService
         public GPTService(string gptModel="gpt-4o-mini", string imageModel="dall-e-3")
         {
-            _chatClient = new ChatClient(
+            _chatClient = new (
                 model: gptModel,
                 apiKey: Environment.GetEnvironmentVariable("OPENAI_APIKEY"));
-            _imagelient = new (
+            _imageclient = new (
                 model: imageModel,
                 apiKey: Environment.GetEnvironmentVariable("OPENAI_APIKEY"));
+            ;
         }
         #endregion
         #region [02] Chat Completion method - streaming mode
@@ -35,11 +39,32 @@ namespace BlazorChatGPT.Services
         }
         #endregion
         #region [03] Generating Images using OpenAI´s Image API
-        public async Task<GeneratedImage> GenerateImageAsync(string prompt)
+        public async Task<GeneratedImage> GenerateImageToUriAsync(string prompt)
         {
-            GeneratedImage image = await _imagelient.GenerateImageAsync(prompt);
+            ImageGenerationOptions options = new()
+                {
+                    Quality= GeneratedImageQuality.High,
+                    Size = GeneratedImageSize.W1024xH1024,
+                    Style = GeneratedImageStyle.Natural,
+                    ResponseFormat = GeneratedImageFormat.Uri
+            };
+            GeneratedImage image = await _imageclient.GenerateImageAsync(prompt,options);
             return image;
         }
+        public async Task<GeneratedImage> GenerateImageToBytesAsync(string prompt)
+        {
+            ImageGenerationOptions options = new()
+                {
+                    Quality= GeneratedImageQuality.High,
+                    Size = GeneratedImageSize.W1024xH1024,
+                    Style = GeneratedImageStyle.Natural,
+                    ResponseFormat = GeneratedImageFormat.Bytes
+            };
+            GeneratedImage image = await _imageclient.GenerateImageAsync(prompt,options);
+            return image;
+        }
+        
+       
         #endregion
     }
 }
